819a5573fdd2edaee1ffc1d1fa812b682d851ed7
Elena <elena.s.demina@gmail.com>
2023-07-25 21:36:01 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // полезное что-то
+
+    // обязана вернуть JSX
+    return (
+        <div>
+            This is App component.
+            <Rating />
+            <Accordion />
+        </div>
+    );
 }
+
+function Star() {
+    return (
+        <div>
+            <div>star</div>
+            <div>star</div>
+            <div>star</div>
+        </div>
+    );
+
+}
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star />
+        </div>
+    );
+}
+
+
+function Accordion() {
+    return <div>
+        <h3>Меню</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+
+}
+
 
 export default App;
