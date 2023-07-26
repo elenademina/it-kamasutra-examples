2ff8c00abc8a470e4b41b2bd99d60b57df4cdd95
Elena <elena.s.demina@gmail.com>
2023-07-26 20:41:04 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
     console.log("App rendering")
@@ -8,53 +10,30 @@ function App() {
     // обязана вернуть JSX
     return (
         <div>
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
+            <PageTitle title={"This is App component."}/>
+            <PageTitle title={"My friends."}/>
+            Article 1
+            <Rating value={3}/>
+            <Accordion titleValue={"Menu"} collapsed={true}/>
+            <Accordion titleValue={"Users"} collapsed={false}/>
+            Article 2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    console.log("AppTitle rendering")
-    return <>This is App component.</>
+type PageTitlePropsType = {
+    title: string
 }
 
-function Star() {
-    console.log("Star rendering")
-    return (
-        <>
-            <div>star</div>
-        </>
-    );
-
-}
-
-function Rating() {
-    console.log("Rating rendering")
-    return (
-        <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </div>
-    );
-}
-
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return <div>
-        <h3>Меню</h3>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    </div>
-
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
+    return <h1>{props.title}</h1>
 }
 
 export default App;
